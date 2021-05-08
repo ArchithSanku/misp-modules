677f0829605d7b80e96be625015a84028c9a4613
ArchithSanku <68359963+ArchithSanku@users.noreply.github.com>
2021-05-08 13:14:00 +0530

    URL

--- misp_modules/modules/expansion/farsight_passivedns.py
+++ misp_modules/modules/expansion/farsight_passivedns.py
@@ -5,10 +5,10 @@ from . import check_input_attribute, standard_error_message
 from datetime import datetime
 from pymisp import MISPEvent, MISPObject, PyMISP
 
-# misp_url = 'https://18.116.32.112/'
-# misp_key = 'uU7TIbeQlAquNHkMfcZyFAkZHoY3hi0mexahbzcR' # The MISP auth key can be found on the MISP web interface under the automation section
-# misp_verifycert = False
-# misp = PyMISP(misp_url, misp_key, misp_verifycert)
+misp_url = 'https://3.15.144.141/'
+misp_key = 'uU7TIbeQlAquNHkMfcZyFAkZHoY3hi0mexahbzcR' # The MISP auth key can be found on the MISP web interface under the automation section
+misp_verifycert = False
+misp = PyMISP(misp_url, misp_key, misp_verifycert)
 
 
 farsight_sharing_group = '88a55e33-9d40-4af0-8985-d91863d42b4b'
@@ -122,9 +122,9 @@ class FarsightDnsdbParser():
             fs_distribution = '5'
         log.debug(event_id)
         log.debug(str(event_id))
-        #misp = PyMISP(misp_url, misp_key, misp_verifycert)
-        #event_details = misp.get(event_id)
-        #log.debug(event_details)
+        misp = PyMISP(misp_url, misp_key, misp_verifycert)
+        event_details = misp.get(event_id)
+        log.debug(event_details)
         for query_type, results in query_response.items():
             comment = self.comment % (query_type, TYPE_TO_FEATURE[self.attribute['type']], self.attribute['value'])
             for result in results:
