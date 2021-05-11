662942e29ec07f65ce536c174e197d520e02258a
ArchithSanku <68359963+ArchithSanku@users.noreply.github.com>
2021-05-11 17:03:48 +0530

    ----

--- misp_modules/modules/expansion/farsight_passivedns.py
+++ misp_modules/modules/expansion/farsight_passivedns.py
@@ -1,50 +1,28 @@
-#!/usr/bin/env python3
-
-import sys
-import logging
 import urllib3
-# import urllib3pip
 from pymisp import PyMISP, MISPEvent, MISPObject
-# from keys import misp_url, misp_key, misp_verifycert
 
-misp_url = 'https://18.116.32.112/'
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+
+misp_url = 'https://18.116.32.112'
 misp_key = 'uU7TIbeQlAquNHkMfcZyFAkZHoY3hi0mexahbzcR'
 misp_verifycert = False
 
-logging.basicConfig(filename = "/home/ubuntu/xyz.txt", filemode = 'a', format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s', datefmt = '%Y-%m-%d %H:%M:%S')
-log = logging.getLogger('xyz')
-log.setLevel(logging.DEBUG)
+
 misp = PyMISP(misp_url, misp_key, misp_verifycert)
-log.debug("Am Reay!!")
 
-# urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-
-
-# misp = PyMISP(misp_url, misp_key, misp_verifycert)
-log.debug("Skipped!")
-# if not misp:
-#     print('PyMISP() failed\n', file=sys.stderr)
-#     sys.exit(1)
 
 misp_evt = MISPEvent()
-misp_evt.distribution = 3  # sharing group
+misp_evt.distribution = 4  # sharing group
 misp_evt.published = False
 misp_evt.sharing_group_id = 1  # depends on your instance
-misp_evt.info = 'Testnnn event with an object'
+misp_evt.info = 'pycharm tTest'
 response = misp.add_event(misp_evt, pythonify=True)
-# if isinstance(response, dict) and 'errors' in response:
-#     print('add_event failed: {}'.format(response['errors']), file=sys.stderr)
-#     sys.exit(2)
-# success
+
 misp_evt = response
-# print('created evt with ID {}'.format(misp_evt.id))
 
 misp_obj = MISPObject(name='blog', strict=True)  # standard template
 misp_obj.add_attribute('post', value='some l33t text')
 response = misp.add_object(misp_evt.id, misp_obj, pythonify=True)
-# if isinstance(response, dict) and 'errors' in response:
-#     print('add_object failed: {}'.format(response['errors']), file=sys.stderr)
-#     sys.exit(2)
-# success
+
 misp_obj = response
-# print('created and added obj with ID {}'.format(misp_obj['id']))
