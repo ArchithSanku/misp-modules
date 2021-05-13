1558596fbacdde734a1981a66838a826ed052a63
ArchithSanku <68359963+ArchithSanku@users.noreply.github.com>
2021-05-13 11:41:16 +0530

    'MISPEvent'

--- misp_modules/modules/expansion/farsight_passivedns.py
+++ misp_modules/modules/expansion/farsight_passivedns.py
@@ -121,7 +121,7 @@ class FarsightDnsdbParser():
                 event = json.loads(self.misp_event.to_json())
                 log.debug(event)
                 event_id = event['uuid']
-                event_distribution = self.misp_event.Distribution
+                event_distribution = self.misp_event.distribution
                 if event_distribution == Distribution.inherit.value:
                     sharing_uuid = self.misp_event.SharingGroup.uuid
                 if sharing_uuid == Farsight_Shared_Group:
@@ -129,7 +129,7 @@ class FarsightDnsdbParser():
                 log.debug(event_id)
                 log.debug(str(event_id))
                 event_details = misp.get(event_id)
-                log.debug(event_details)
+                log.debug(str(event_details))
                                                 
 #                 event_distribution = self.misp_event.distribution
 #                 if event_distribution == Distribution.inherit.value :
